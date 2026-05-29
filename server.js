e839223124c62c9050ca842c7d18c0495c89d777
viewerok2 <gonzagoni@yahoo.com>
2026-05-29 04:31:02 -0300

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ res.end(data);
 }
 
 else if (req.url === '/imagen.png') {
-fs.readFile('logomp.png', (err, data) => {
+fs.readFile('imagen.png', (err, data) => {
 res.writeHead(200, { 'Content-Type': 'image/png' });
 res.end(data);
 });
